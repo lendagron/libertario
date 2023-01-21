88bd3155cac1ddb597baaf37723f19100e7e9140
Gustavo <rogupoletti@hotmail.com>
2023-01-21 18:10:39 -0300

    adicionadas rotas personalizadas

---
+++ src/pages/course/[course_slug].tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import React from "react";
+import CursoContent from "../../components/curso/CursoContent";
+import { useRouter } from 'next/router'
+
+export default function Course() {
+  const router = useRouter()
+  const { course_slug } = router.query
+  return (
+    <div>
+      <Head>
+        <title>{course_slug} aaa</title>
+        {/*  <link rel='icon' href='/favicon.ico' /> */}
+      </Head>
+      <CursoContent />
+    </div>
+  );
+}
