574fcb028c3250f33f94a483e3ea4c8bb79c99ba
Matheus Lima <matheus.lima@ccc.ufcg.edu.br>
2023-05-03 20:10:53 -0300

    fix: trocar senha permissão da página

--- src/pages/trocar.tsx
+++ src/pages/trocar.tsx
@@ -1,21 +1,23 @@
- import React from "react";
- import Head from "next/head";
- import Changepswd from '../components/changepswd/Changepswd';
- import { HeaderLogged } from '../components/headerLogged/HeaderLogged';
+import React from "react";
+import Head from "next/head";
+import Changepswd from "../components/changepswd/Changepswd";
+import { HeaderLogged } from "../components/headerLogged/HeaderLogged";
+import { withSSRAuth } from "../utils/withSSRAuth";
 
+export default function trocar() {
+  return (
+    <>
+      <Head>
+        <title>Clube da liberdade | Mudar Senha</title>
+      </Head>
+      <HeaderLogged isPainel={false} />
+      <Changepswd />
+    </>
+  );
+}
 
- export default function trocar(){
-       return (
-           <>
-             <Head>
-                <title>Clube da liberdade | Mudar Senha</title>
-             </Head>
-             <HeaderLogged isPainel={false} />
-             <Changepswd />
-           </>
-       );
- }
- 
-
-
-
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  return {
+    props: {},
+  };
+});
