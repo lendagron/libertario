9637f2ccfb4b2a643f76af5c76bbc383cdf83238
Matheus Lima <matheus.lima@ccc.ufcg.edu.br>
2023-06-06 20:56:16 -0300

    bug: página deletada (?) e refeita

--- src/components/planosContent/PlanosContent.tsx
+++ src/components/planosContent/PlanosContent.tsx
@@ -1,8 +1,8 @@
-import { Button, Card, Divider, Grid, Link, Text } from '@nextui-org/react';
-import React from 'react';
+import { Button, Card, Divider, Grid, Link, Text } from "@nextui-org/react";
+import React from "react";
 import { Check } from "phosphor-react";
-import { Box } from '../styles/box';
-import { Flex } from '../styles/flex';
+import { Box } from "../styles/box";
+import { Flex } from "../styles/flex";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import styles from "./planosContent.module.scss";
@@ -11,79 +11,75 @@ export default function PlanosContent() {
   const { isAuthenticated } = useContext(AuthContext);
   return (
     <>
-       <Flex
-          css={{py: '$20', gap: '1rem', px: '$6'}}
-          justify={'center'}
-          wrap={'wrap'}
-          direction={'column'}
-          align={'center'}
-       >
-          <Flex direction={'column'} align={'center'}>
-             <Text span css={{color: '$blue600'}}>
-                Ajude a construir uma sociedade mais livre.
-             </Text>
-             <Text h1>Escolha seu Plano</Text>
-          </Flex>
+      <Flex
+        css={{ py: "$20", gap: "1rem", px: "$6" }}
+        justify={"center"}
+        wrap={"wrap"}
+        direction={"column"}
+        align={"center"}
+      >
+        <Flex direction={"column"} align={"center"}>
+          <Text span css={{ color: "$blue600" }}>
+            Ajude a construir uma sociedade mais livre.
+          </Text>
+          <Text h1>Escolha seu Plano</Text>
+        </Flex>
 
-          <Flex
-             css={{gap: '2rem', width: '100%'}}
-             wrap={'wrap'}
-             justify={'center'}
-          >
-          <Card css={{ p: '$6', mw: '400px' }}>
+        <Flex
+          css={{ gap: "2rem", width: "100%" }}
+          wrap={"wrap"}
+          justify={"center"}
+        >
+          <Card css={{ p: "$6", mw: "400px" }}>
             <Card.Header>
-              <Grid.Container css={{ pl: '$6' }}>
+              <Grid.Container css={{ pl: "$6" }}>
                 <Grid xs={12}>
-                  <Text h4 css={{ lineHeight: '$xs' }}>
+                  <Text h4 css={{ lineHeight: "$xs" }}>
                     Mises
                   </Text>
                 </Grid>
                 <Grid xs={12}>
-                  <Text css={{ color: '$accents8' }}>
-                    Descrição Teste
-                  </Text>
+                  <Text css={{ color: "$accents8" }}>Descrição Teste</Text>
                 </Grid>
               </Grid.Container>
             </Card.Header>
-            <Card.Body css={{ py: '$2' }}>
-              <Text css={{ display: 'contents' }} h2>
-                R$ 10{' '}
+            <Card.Body css={{ py: "$2" }}>
+              <Text css={{ display: "contents" }} h2>
+                R$ 10{" "}
               </Text>
-              <Text css={{ display: 'contents', color: '$accents8' }}>
+              <Text css={{ display: "contents", color: "$accents8" }}>
                 /mês
               </Text>
-              <Button css={{ mt: '$7', mb: '$12'}} href="/" as="a">Acesse</Button>
+              <Button css={{ mt: "$7", mb: "$12" }} href='/' as='a'>
+                Acesse
+              </Button>
 
               <Divider />
-              <Box as={'ul'}>
-                <Flex
-                  as={'li'}
-                  css={{ py: '$2', gap: '$2' }}
-                  align={'center'}
-                >
+              <Box as={"ul"}>
+                <Flex as={"li"} css={{ py: "$2", gap: "$2" }} align={"center"}>
                   <Check />
-                  <Text span css={{ color: '$accents8' }}>
+                  <Text span css={{ color: "$accents8" }}>
                     Vantagem 1
                   </Text>
                 </Flex>
                 <Flex
-                  as={'li'}
-                  css={{ py: '$2', gap: '$2', textDecoration: 'line-through' }}
-                  align={'center'}
+                  as={"li"}
+                  css={{ py: "$2", gap: "$2", textDecoration: "line-through" }}
+                  align={"center"}
                 >
                   <Check />
-                  <Text span css={{ color: '$accents8' }}>
+                  <Text span css={{ color: "$accents8" }}>
                     Vantagem 2
                   </Text>
                 </Flex>
               </Box>
             </Card.Body>
           </Card>
-          </Flex>
-       </Flex>
-       <Divider
-          css={{position: 'absolute', inset: '0p', left: '0', mt: '$5'}}
-       />
+        </Flex>
+      </Flex>
+      <Divider
+        css={{ position: "absolute", inset: "0p", left: "0", mt: "$5" }}
+      />
     </>
   );
 }
